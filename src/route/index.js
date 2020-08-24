5e9b6b74f386f29358482152e3acdb4c3ee56831
Sardor <mukhtorov_1994@mail.ru>
2020-08-24 20:37:56 +0500

    routes

---
+++ src/route/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Hojiakbar from '../components/Hojiakbar'
+import Rivojiddin from '../components/Rivojiddin'
+import Habibullo from '../components/Habibullo'
+import Sardor from '../components/Sardor'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/rivojiddin" component={Rivojiddin} />
+        <Route path="/hojiakbar" component={Hojiakbar} />
+        <Route path="/habibullo" component={Habibullo} />
+        <Route path="/sardor" component={Sardor} />
+      </Switch>
+    </Router>
+  )
+}

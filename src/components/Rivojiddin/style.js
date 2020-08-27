fc94d5911712ee0574488c2faba19ec16c5dd74e
din0497 <rivojidin97@gmail.com>
2020-08-27 22:42:56 +0500

    review

--- src/components/Rivojiddin/style.js
+++ src/components/Rivojiddin/style.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components'
 
+export const Wrapper = styled.div`
+display:flex;
+justify-content: space-evenly;
+`
+
+
 export const Container = styled.div`
 display:flex;
 flex-direction:row;
-max-width: 545px;
-min-width: 545px;
+width: 545px;
+width: 545px;
 height: 695px;
-margin: auto;
 
 background: #FFFFFF;
 border: 1px solid #000000;
@@ -14,6 +19,24 @@ box-sizing: border-box;
 filter: drop-shadow(0px 6px 12px rgba(73, 75, 116, 0.04));
 box-shadow: 0px 6px 12px rgba(73, 75, 116, 0.16);
 `
+Container.Form = styled.div`
+display:flex;
+flex-direction: column;
+`
+
 export const Side = styled.div`
+
+`
+
+export const Input = styled.input`
+width: 100%;
+height:43px;
+border-radius: 8px;
+background: #f8f8f8;
+border:none;
+font-size: 22px;
+padding-left: 15px;
+margin: 10px;
+
 
 `

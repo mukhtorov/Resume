174e313ae3ee1c8786f50e3f3907d44fd08fdcc6
Sardor <mukhtorov_1994@mail.ru>
2020-09-01 15:46:14 +0500

    last

--- src/components/Sardor/index.js
+++ src/components/Sardor/index.js
@@ -1,94 +1,96 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Container, Wrapper, Div, DivContainer, Form, Input, LangContainer } from './style'
 import Sidebar from './Sidebar'
 import Body from './Body'
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contact: "",
-      emailTitle: '',
-      lang: [
-        {
-          id: 1,
-          lang: "English",
-          lev: "Native"
-        },
-        {
-          id: 2,
-          lang: "Uzbek",
-          lev: "Native"
-        },
-        {
-          id: 3,
-          lang: "Kazak",
-          lev: "Native"
-        }
-      ]
-    }
-  }
-  Add = (params) => {
-    this.setState({
-      lang: this.state.lang.concat({
-        id: this.state.lang.length + 1,
-        lang: "",
-        lev: ''
-      })
-    })
-  }
+import GoogleMapReact from 'google-map-react';
 
 
-  onChange = (e, id) => {
-    const { target: { value, name } } = e;
-    this.setState((state) => {
-      const list = state.lang.map((item) => {
-        if (item.id == id) {
-          if (name === 'lang') {
-            item.lang = value
-          }
-          if (name === 'lev') {
-            item.lev = value;
-          }
-        }
-      })
-      return {
-        list
-      }
-    }
-    )
-  }
-  render() {
-    return (
-      <Wrapper>
-        <Container>
-          <Sidebar data={this.state} />
-          <Body />
-        </Container>
-        {/* FORM */}
-        <Form>
-          <Input placeholder="Contact" onChange={(e) => this.setState({ contact: e.target.value })} />
-          <LangContainer>
-            {
-              this.state.lang.map((lan, index) => {
-                return (
-                  <div style={{ display: 'flex' }}>
-                    <Input placeholder="Language" name="lang" value={lan.lang} onChange={(e) => this.onChange(e, index + 1)} />
-                    <Input placeholder="Level" name="lev" value={lan.lev} onChange={(e) => this.onChange(e, index + 1)} />
-                  </div>
-                )
-              })
-            }
-            <button onClick={() => this.Add()}>more lang</button>
-          </LangContainer>
-        </Form>
-      </Wrapper>
-    )
-  }
-}
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       contact: "",
+//       emailTitle: '',
+//       lang: [
+//         {
+//           id: 1,
+//           lang: "English",
+//           lev: "Native"
+//         },
+//         {
+//           id: 2,
+//           lang: "Uzbek",
+//           lev: "Native"
+//         },
+//         {
+//           id: 3,
+//           lang: "Kazak",
+//           lev: "Native"
+//         }
+//       ]
+//     }
+//   }
+//   Add = (params) => {
+//     this.setState({
+//       lang: this.state.lang.concat({
+//         id: this.state.lang.length + 1,
+//         lang: "",
+//         lev: ''
+//       })
+//     })
+//   }
 
-export default App
+
+//   onChange = (e, id) => {
+//     const { target: { value, name } } = e;
+//     this.setState((state) => {
+//       const list = state.lang.map((item) => {
+//         if (item.id == id) {
+//           if (name === 'lang') {
+//             item.lang = value
+//           }
+//           if (name === 'lev') {
+//             item.lev = value;
+//           }
+//         }
+//       })
+//       return {
+//         list
+//       }
+//     }
+//     )
+//   }
+//   render() {
+//     return (
+//       <Wrapper>
+//         <Container>
+//           <Sidebar data={this.state} />
+//           <Body />
+//         </Container>
+//         {/* FORM */}
+//         <Form>
+//           <Input placeholder="Contact" onChange={(e) => this.setState({ contact: e.target.value })} />
+//           <LangContainer>
+//             {
+//               this.state.lang.map((lan, index) => {
+//                 return (
+//                   <div style={{ display: 'flex' }}>
+//                     <Input placeholder="Language" name="lang" value={lan.lang} onChange={(e) => this.onChange(e, index + 1)} />
+//                     <Input placeholder="Level" name="lev" value={lan.lev} onChange={(e) => this.onChange(e, index + 1)} />
+//                   </div>
+//                 )
+//               })
+//             }
+//             <button onClick={() => this.Add()}>more lang</button>
+//           </LangContainer>
+//         </Form>
+//       </Wrapper>
+//     )
+//   }
+// }
+
+// export default App
 
 
 // import React from 'react'
@@ -141,3 +143,38 @@ export default App
 // export default Demo
 
 
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCdZ_-DKhZ_hTQGJDF7l2CgfhntCX-11k0' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          yesIWantToUseGoogleMapApiInternals
+        // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
